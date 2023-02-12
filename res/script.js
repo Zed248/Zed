047d72ba0cd380b7d7a75371358e184d653d20c7
Rakkha <linnayfb@gmail.com>
2023-02-12 09:25:26 -0800

    This is sub intitializing

--- res/script.js
+++ res/script.js
@@ -28,7 +28,7 @@ var logInPasswordInput = document.querySelector('#input-sign-in-password');
 var logInButton = document.querySelector('#sign-in-btn');
 
 //msgBox
-var msgBox = document.querySelector('.msgbox');
+var msgBox = document.querySelector('.msg-box');
 //Sign in Validation
 logInButton.addEventListener('click', () => {
 
@@ -37,17 +37,24 @@ logInButton.addEventListener('click', () => {
 
     //username checker
     if (logInUserName == getLogInUserName && logInPassword == getLogInPassword) {
-        alert("Success....!");
-        msgBox.innerHTML = "User Name : " + getLogInUserName + "Password : " + getLogInPassword;
-        logInModelBox.classList.add('active');
+        msgBox.innerHTML = "User Name : " + getLogInUserName + "| Password : " + getLogInPassword;
+        logInModelBox.style.display = "none";
+        logInNavToolCaller.innerHTML = "Admin Account";
+        logInNavToolCaller.style.cssText = "background-color: green; color: white";
+        msgBox.classList.add('active');
+        alert("You have access Admin User Account!" + " Username: " + logInUserName + "| Password: " + logInPassword + "! You can enjoy using our blog. Thanks!")
+        console.log("click log-in true")
     } else {
-        alert("Tryagain....!");
-        logInModelBox.style.display = "flex"
+        msgBox.innerHTML = "Wrong Entry Information! Try again!";
+        logInModelBox.style.display = "flex";
+        // location.reload();
+        console.log("click log-in but wrong")
     }
 })
 
 
 //open ModelBox Back
+var signUpNavigatorForSignIn = document.querySelector('#sign-up-navigator');
 var closeLogInModelBtn = document.querySelector('#close');
 var logInNavToolCaller = document.querySelector('.sign-in-btn-show');
 
@@ -57,4 +64,29 @@ logInNavToolCaller.addEventListener('click', () => {
 
 closeLogInModelBtn.addEventListener('click', () => {
     logInModelBox.style.display = "none";
+})
+
+signUpNavigatorForSignIn.addEventListener('click', () => {
+    signUpSection.classList.add('active');
+    signInSection.classList.remove('active');
+    signUpSectionHeading.classList.add('active');
+    signInSectionHeading.classList.remove('active');
+})
+
+
+
+//Hamburger Btn action
+const hamburger = document.querySelector('.hamburger');
+const hamBtn = document.querySelector('.hamburger-btn');
+
+let menuOpen = false;
+
+hamburger.addEventListener('click', () => {
+    if(!menuOpen) {
+        hamBtn.classList.add('active');
+        menuOpen = true;
+    } else {
+        hamBtn.classList.remove('active');
+        menuOpen = false;
+    }
 })
